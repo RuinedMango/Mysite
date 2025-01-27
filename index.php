4e58075b19a162f3a1324ebe0c6a7b4bb707af44
Ruinedmango <00rudystx@gmail.com>
2025-01-26 18:23:38 -0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,
+                                   initial-scale=1.0">
+    <title>My Project</title>
+
+    <!-- To link external styling file -->
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <header>
+        <h1>
+            <?php echo "I have nutsack cancer" ?>
+            Welcome to My site
+        </h1>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>My Project</h2>
+                    <p>
+                        This is where your content goes.
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <h2>About Me</h2>
+                    <p>
+                        I'm a geek with a nutsack cancer.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <p>&copy; 2024 My Project</p>
+    </footer>
+</body>
+
+</html>
